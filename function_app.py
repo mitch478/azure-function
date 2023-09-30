bd7f34ed7572026da5daaf4d8a5ae1c0893ef0ad
Nathan Mitchell <nathan@mcsltd.io>
2023-09-30 09:37:02 +0100

    Added first app

--- function_app.py
+++ function_app.py
@@ -1,25 +1,29 @@
 import azure.functions as func
 import logging
 
-app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
+# Define the FunctionApp instance
+app = func.FunctionApp()
 
-@app.route(route="YT-Downloader")
-def YT-Downloader(req: func.HttpRequest) -> func.HttpResponse:
-logging.info('Python HTTP trigger function processed a request.')
+# Define the route for the function
+@app.route(route="YT-Downloader", methods=["GET", "POST"])
+def YT_Downloader(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
 
+    # Get the 'name' parameter from the query string
     name = req.params.get('name')
+
     if not name:
         try:
+            # Try to parse JSON from the request body
             req_body = req.get_json()
+            name = req_body.get('name')
         except ValueError:
             pass
-        else:
-            name = req_body.get('name')
 
     if name:
         return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
     else:
         return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
+            "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+            status_code=200
         )
